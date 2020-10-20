1b048ae5d8e9735f57b4f3e6be8f2cf39dada910
dunirs <1662354630@qq.com>
2020-10-20 20:13:15 +0800

    字典学习

---
+++ dict/dict01_10.20.py
@@ -0,0 +1,18 @@
+"""
+字典
+特点：
+1.符号：{}
+2.关键字：dict
+3、保存的元素是：key：value
+"""
+
+
+# 定义
+dict1 = {}   # 空字典
+dict2 = dict()  # 空字典
+dict3 = {'ID': '123', 'name': 'lucy', 'age': 18}
+dict4 = dict([('name', 'lucy'), ('age', 18)])  # 里面元素必须成对（两个）出现
+print(dict4)
+
+# 字典的增删改查:
+
